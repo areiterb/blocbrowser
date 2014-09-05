4256f86d3bd08a80756677db8b53c3ad5e3f91a2
Alejandro <alejandroreiter@gmail.com>
2014-09-05 14:57:59 -0500

    Initial Commit

---
+++ BlocBrowser/BLCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BLCAppDelegate.h
+//  BlocBrowser
+//
+//  Created by ALEJANDRO REITER B on 9/5/14.
+//  Copyright (c) 2014 Alejandro Reiter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
